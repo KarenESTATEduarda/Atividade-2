955903287b3fc54855261920ff5e84cc2d0233e8
KarenESTATEduarda <74926693+KarenESTATEduarda@users.noreply.github.com>
2021-06-15 15:53:15 -0300

    Create Atividade2.java

---
+++ Atividade2.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class ATIVIDADE2 {
+        
+
+    
+    public static void main(String[] args) {
+        Aluno aluno1 = new Aluno();
+        Disciplina disciplina = new Disciplina();
+        
+        Scanner sc= new Scanner(System.in); 
+               
+        System.out.println("Digite um nome: ");
+        String a = sc.nextLine();        
+        aluno1.alteraNome(a);
+        
+        System.out.println("Digite um id: ");
+        int b = sc.nextInt();
+        aluno1.alteraId(b);
+        
+        aluno1.nota1 = 3.5;
+        aluno1.nota2 = 6.5;
+        
+        System.out.println("A média do aluno é " + aluno1.retornaMedia());
+       
+        if (aluno1.passou()){
+            System.out.println("O aluno foi aprovado por média");
+        } else{
+            System.out.println("O aluno não foi aprovado por média");
+        }
+        
+        System.out.println("Populando array...");
+        disciplina.populaArray();
+        
+        System.out.println("Digite uma posição na lista para imprimir o sucessor e antecessor");
+        int c = sc.nextInt();
+        disciplina.antecessorSucessor(c);
+    
+    }
+    
+    
+}
